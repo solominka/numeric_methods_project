9b8a69baf5806f2458330b7c74c4bba018b43a42
anna.kolesnikova <anna.kolesnikova@aliexpress.ru>
2022-11-21 17:57:58 +0300

    add gauss_elimination

--- direct_methods.py
+++ direct_methods.py
@@ -3,7 +3,7 @@ import numpy as np
 from common import solve_upper_triangular_matrix
 
 
-def gauss_simple(extended_matrix):
+def gauss_elimination(extended_matrix):
     assert len(extended_matrix) > 0
     assert len(extended_matrix) + 1 == len(extended_matrix[0])
     np_matrix = np.array(extended_matrix, dtype=float)
@@ -11,8 +11,15 @@ def gauss_simple(extended_matrix):
         pivot = cur_row[i]
         if pivot != 0:
             cur_row /= float(pivot)
-
-        for next_row in np_matrix[i+1:]:
-            next_row -= next_row[i] * cur_row
+        fill_by_rectangle_rule(np_matrix, i)
 
     return solve_upper_triangular_matrix(np_matrix)
+
+
+def fill_by_rectangle_rule(extended_matrix, cur_iteration):
+    n = len(extended_matrix)
+    for i in range(cur_iteration+1, n):
+        k = extended_matrix[cur_iteration][cur_iteration]
+        for j in range(cur_iteration+1, n+1):
+            extended_matrix[i][j] -= extended_matrix[cur_iteration][j] * extended_matrix[i][cur_iteration] / k
+        extended_matrix[i][cur_iteration] = 0
--- test_basic.py
+++ test_basic.py
@@ -1,17 +1,17 @@
 import unittest
 
-from direct_methods import gauss_simple
+from direct_methods import gauss_elimination
 
 EPS = 0.01
 
 
 class TestCases(unittest.TestCase):
-    def test_gauss_simple(self):
-        matrix = [[5, 0, 1, 11], [2, 6, -2, 5], [-3, 2, 10, 6]]
-        ans = gauss_simple(matrix)
-        assert abs(ans[0] - 1.98) < EPS
-        assert abs(ans[1] - 0.54) < EPS
-        assert abs(ans[2] - 1.09) < EPS
+    def test_gauss_elimination(self):
+        matrix = [[2, 1, 4, 16], [3, 2, 1, 10], [1, 3, 3, 16]]
+        ans = gauss_elimination(matrix)
+        assert abs(ans[0] - 1) < EPS
+        assert abs(ans[1] - 2) < EPS
+        assert abs(ans[2] - 3) < EPS
 
 
 if __name__ == '__main__':
