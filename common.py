607d13442d6404e264c552c7be1ac61bea00ee72
anna.kolesnikova <anna.kolesnikova@aliexpress.ru>
2022-11-21 20:30:54 +0300

    add LU_decomposition

--- common.py
+++ common.py
@@ -6,3 +6,14 @@ def solve_upper_triangular_matrix(extended_matrix):
         for j in range(i+1, n):
             solution[i] -= extended_matrix[i][j] * solution[j]
     return solution
+
+
+def solve_lower_triangular_matrix(extended_matrix):
+    n = len(extended_matrix)
+    solution = [0] * n
+    for i in range(0, n):
+        solution[i] = extended_matrix[i][n]
+        for j in range(0, i):
+            solution[i] -= extended_matrix[i][j] * solution[j]
+        solution[i] /= extended_matrix[i][i]
+    return solution
--- direct_methods.py
+++ direct_methods.py
@@ -1,18 +1,29 @@
 import numpy as np
 
-from common import solve_upper_triangular_matrix
+from common import solve_upper_triangular_matrix, solve_lower_triangular_matrix
 
 
-def gauss_simple(extended_matrix):
+def LU_decomposition(extended_matrix):
     assert len(extended_matrix) > 0
     assert len(extended_matrix) + 1 == len(extended_matrix[0])
-    np_matrix = np.array(extended_matrix, dtype=float)
-    for i, cur_row in enumerate(np_matrix):
-        pivot = cur_row[i]
-        if pivot != 0:
-            cur_row /= float(pivot)
 
-        for next_row in np_matrix[i+1:]:
-            next_row -= next_row[i] * cur_row
+    n = len(extended_matrix)
+    lu_matrix = np.array(extended_matrix, dtype=float)
+    for k in range(0, n):
+        for i in range(k, n):
+            sum = 0
+            for s in range(0, k):
+                sum += lu_matrix[i, s] * lu_matrix[s, k]
+            lu_matrix[i, k] -= sum
+        for j in range(k + 1, n):
+            sum = 0
+            for s in range(0, k):
+                sum += lu_matrix[k, s] * lu_matrix[s, j]
+            lu_matrix[k, j] = (lu_matrix[k, j] - sum) / lu_matrix[k, k]
 
-    return solve_upper_triangular_matrix(np_matrix)
+    y = solve_lower_triangular_matrix(lu_matrix)
+
+    for i in range(0, n):
+        lu_matrix[i, i] = 1
+        lu_matrix[i, n] = y[i]
+    return solve_upper_triangular_matrix(lu_matrix)
