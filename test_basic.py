7766aa7b25da968c54a091a767c74ab0d9322873
anna.kolesnikova <anna.kolesnikova@aliexpress.ru>
2022-11-21 21:54:09 +0300

    add simple_iteration

--- test_basic.py
+++ test_basic.py
@@ -1,9 +1,17 @@
 import unittest
 
+from iterative_methods import simple_iteration
+
+EPS = 0.001
+
 
 class TestCases(unittest.TestCase):
-    def test(self):
-        assert 1 == 1
+    def test_simple_iteration(self):
+        matrix = [[10, 1, 1, 12], [2, 2, 10, 14], [2, 10, 1, 13]]
+        ans = simple_iteration(matrix, EPS)
+        assert abs(ans[0] - 1) < EPS
+        assert abs(ans[1] - 1) < EPS
+        assert abs(ans[2] - 1) < EPS
 
 
 if __name__ == '__main__':
