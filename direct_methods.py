0848a17fd0749081477de4ed5c1f938e601e53ee
Anna Kolesnikova <46619453+solominka@users.noreply.github.com>
2022-11-22 17:57:19 +0300

    add gauss_choice (#3)
    Co-authored-by: anna.kolesnikova <anna.kolesnikova@aliexpress.ru>

---
+++ direct_methods.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from common import solve_upper_triangular_matrix, fill_by_rectangle_rule
+
+
+def gauss_choice(extended_matrix):
+    assert len(extended_matrix) > 0
+    assert len(extended_matrix) + 1 == len(extended_matrix[0])
+
+    np_matrix = np.array(extended_matrix, dtype=float)
+    for i, row in enumerate(np_matrix):
+        max_index = i + np.argmax(abs(np_matrix[i:, i]))
+        if max_index != i:
+            np_matrix[[i, max_index]] = np_matrix[[max_index, i]]
+        if row[i] != 0:
+            row /= float(row[i])
+        fill_by_rectangle_rule(np_matrix, i)
+    return solve_upper_triangular_matrix(np_matrix)
--- test_basic.py
+++ test_basic.py
@@ -1,9 +1,17 @@
 import unittest
 
+from direct_methods import gauss_choice
+
+EPS = 0.01
+
 
 class TestCases(unittest.TestCase):
-    def test(self):
-        assert 1 == 1
+    def test_gauss_choice(self):
+        matrix = [[-3, 2.099, 6, 3.901], [10, -7, 0, 7], [5, -1, 5, 6]]
+        ans = gauss_choice(matrix)
+        assert abs(ans[0] - 0) < EPS
+        assert abs(ans[1] + 1) < EPS
+        assert abs(ans[2] - 1) < EPS
 
 
 if __name__ == '__main__':
