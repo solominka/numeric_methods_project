1e3bbd72f991c98d75dbb8ee6b091baa54dc36a3
Anna Kolesnikova <46619453+solominka@users.noreply.github.com>
2022-11-22 21:31:11 +0300

    add tridiagonal_matrix_algorithm (метод прогонки) (#4)

--- direct_methods.py
+++ direct_methods.py
@@ -29,3 +29,24 @@ def gauss_choice(extended_matrix):
             row /= float(row[i])
         fill_by_rectangle_rule(np_matrix, i)
     return solve_upper_triangular_matrix(np_matrix)
+
+
+def tridiagonal_matrix_algorithm(extended_matrix):
+    assert len(extended_matrix) > 0
+    assert len(extended_matrix) + 1 == len(extended_matrix[0])
+
+    n = len(extended_matrix)
+    pq_matrix = np.zeros((n, n+1))
+    pq_matrix[0][1] = extended_matrix[0][1] / extended_matrix[0][0]
+    pq_matrix[0][n] = extended_matrix[0][n] / extended_matrix[0][0]
+    pq_matrix[0][0] = 1
+
+    for i, row in enumerate(extended_matrix):
+        if i + 1 < n:  # P[i]
+            pq_matrix[i][i + 1] = -row[i + 1] / (-row[i] + row[i - 1] * pq_matrix[i - 1][i])
+
+        # Q[i]
+        pq_matrix[i][n] = (row[i - 1] * pq_matrix[i - 1][n] - row[n]) / (-row[i] + row[i - 1] * pq_matrix[i - 1][i])
+        pq_matrix[i][i] = 1
+
+    return solve_upper_triangular_matrix(pq_matrix)
--- test_basic.py
+++ test_basic.py
@@ -1,17 +1,28 @@
 import unittest
 
-from direct_methods import gauss_choice, gauss_elimination
+from direct_methods import gauss_choice, gauss_elimination, tridiagonal_matrix_algorithm
 
 EPS = 0.01
 
 
 class TestCases(unittest.TestCase):
+    def test_tridiagonal_matrix_algorithm(self):
+        matrix = [[5, 3, 0, 0, 8], [3, 6, 1, 0, 10], [0, 1, 4, -2, 3], [0, 0, 1, -3, -2]]
+        ans = tridiagonal_matrix_algorithm(matrix)
+        assert abs(ans[0] - 1) < EPS
+        assert abs(ans[1] - 1) < EPS
+        assert abs(ans[2] - 1) < EPS
+        assert abs(ans[3] - 1) < EPS
+        
+
     def test_gauss_elimination(self):
         matrix = [[2, 1, 4, 16], [3, 2, 1, 10], [1, 3, 3, 16]]
         ans = gauss_elimination(matrix)
         assert abs(ans[0] - 1) < EPS
         assert abs(ans[1] - 2) < EPS
         assert abs(ans[2] - 3) < EPS
+        
+
     def test_gauss_choice(self):
         matrix = [[-3, 2.099, 6, 3.901], [10, -7, 0, 7], [5, -1, 5, 6]]
         ans = gauss_choice(matrix)
